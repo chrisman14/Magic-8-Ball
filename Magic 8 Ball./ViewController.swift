9947309d91d2ae43a20e283bac4909ac7099243b
Chrisman <chrisman.hyutagalung@gmail.com>
2019-07-29 15:16:12 +0700

    push projec

--- Magic 8 Ball./ViewController.swift
+++ Magic 8 Ball./ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var ballArray: [String]=["ball1", "ball2", "ball3", "ball4","ball5"]
+    var randomBallNumber=0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newBallIMage()
     }
-
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallIMage()
+    }
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        newBallIMage()
+    }
+    
+    func newBallIMage(){
+        randomBallNumber = Int.random(in: 0...4)
+        let nameImage=ballArray[randomBallNumber]
+        imageView.image=UIImage(named: nameImage)
+    }
 
 }
 
